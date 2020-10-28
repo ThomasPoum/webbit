3db9b2dcd0d6056322a6abd1601f249f04081314
Thomas POUMAREDE <thomaspoumarede@MacBook-Pro-de-thomas-2.home>
2020-10-28 07:21:43 +0100

    commit avec les modifications de css du layout principal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
     def render_svg(name, styles: "fill-current text-grey-400", title:nil)
         filename = "#{name}.svg"
         title ||= name.underscore.humanize
-        inline_svg_pack_tag(filename, aria: true, nocomment: true, title: title, class: styles)
+        inline_svg_tag(filename, aria: true, nocomment: true, title: title, class: styles)
     end
 end
