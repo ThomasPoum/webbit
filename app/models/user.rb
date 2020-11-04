2a7fa71b55154769988eabfe5a6486f7756ac951
Thomas POUMAREDE <thomaspoumarede@MacBook-Pro-de-thomas-2.home>
2020-11-04 14:16:55 +0100

    end of subscriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :submissions
-  has_many :communities
   has_many :comments
+
+  has_many :subscriptions
+  has_many :communities, through: :subscriptions
 
   validates_uniqueness_of :username
   validates_presence_of :username
+
+  has_many :subscribed_submissions, through: :communities, source: :submissions
 
   acts_as_voter
   
